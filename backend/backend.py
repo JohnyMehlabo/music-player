1c386589dda903746da14cf7f9fe84e2a0e7ae8f
Johny Mehlabo <manestev@gmail.com>
2023-07-19 19:54:31 +0200

    Updated GUI

--- backend/backend.py
+++ backend/backend.py
@@ -7,29 +7,56 @@ p : vlc.MediaPlayer = None
 with open("song_list.json", "r") as f:
     song_list = json.loads(f.read())
 
-
+def playSong(id):
+    global p
+    if p:
+        p.stop()
+    p = vlc.MediaPlayer("songs/" + song_list[id]["filename"])
+    p.play()
 
 def handleConnection(conn : socket.socket, addr):
     global p
+    song_id = -1
+    Ended = 6
+    
     while True:
-        data = conn.recv(1024).decode()
+        data = conn.recv(4096).decode()
         values = data.split(" ")
         command = values[0]
         args = values[1:]
 
+        if p:
+            current_state = p.get_state()
+        else:
+            current_state = - 1
+
+
+        if current_state == Ended:
+            song_id += 1
+            playSong(song_id)
+
+        if command == "get_current_status":
+            print("Command: get_current_status")
+            current_song = song_list[song_id]
+            if p:
+                is_playing = p.is_playing()
+            else:
+                is_playing = 0
+            conn.send(f"{current_song}-{is_playing}".encode())
         if command == "play":
             print("Command: play")
+            song_id = int(args[0])
+            playSong(song_id)
+        if command == "pause":
             if p:
-                p.stop()
-            p = vlc.MediaPlayer("songs/" + song_list[int(args[0])]["filename"])
-            p.play()
+                p.pause()
         if command == "stop":
             print("Command: play")
             if p:
                 p.stop()
         if command == "get_songs":
             print("Command: get_songs")
-            conn.send(json.dumps(song_list).encode())
+            conn.send(json.dumps(song_list).encode("utf-8"))
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1", 4444))
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,24 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import socket
 import json
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(2)
 s.connect(("127.0.0.1", 4444))
 
+def getCurrentStatus():
+    s.send("get_current_status".encode())
+    data = s.recv(1024).decode().split("-")
+    return (json.loads(data[0].replace("'", "\"")), int(data[1]))
+
+
 @app.route("/songs")
 def songs():
     s.send("get_songs".encode())
-    data = json.loads(s.recv(1024).decode())
+    data = json.loads(s.recv(4096).decode("utf-8"))
     context = {
-        songs : data
+        "songs" : data
     }
     return render_template("song_list.html", songs=data)
 
+@app.route("/pause")
+def pause():
+    s.send("pause".encode())
+    return redirect("/")
+
 @app.route("/")
 def index():
-    return "<p>Hello World</p>"
+    currentSong, isPlaying = getCurrentStatus()
+    return render_template("index.html", currentSong=currentSong, isPlaying=isPlaying)
 
 @app.route("/play")
 def play_song():

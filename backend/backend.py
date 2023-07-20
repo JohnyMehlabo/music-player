4938d869b6ac173ac5f797f99085fedee816b8bd
Johny Mehlabo <manestev@gmail.com>
2023-07-20 14:39:32 +0200

    Added playlists

--- backend/backend.py
+++ backend/backend.py
@@ -4,12 +4,22 @@ import json
 from threading import Thread
 
 Ended = 6
-song_id = -1
+song_id = 0
+current_mode = 0
+
+playlist_name = ""
+song_queue = []
+queue_index = []
 
 p : vlc.MediaPlayer = None
 
 with open("song_list.json", "r") as f:
     song_list = json.loads(f.read())
+    f.close()
+
+with open("playlists.json", "r") as f:
+    playlists = json.loads(f.read())
+    f.close()
 
 def playSong(id):
     global p
@@ -19,6 +29,7 @@ def playSong(id):
     p.play()
 
 def handleEnd():
+    global queue_index, song_queue
     try:
         global song_id, p
         while True:
@@ -28,13 +39,26 @@ def handleEnd():
                 current_state = - 1
 
             if current_state == Ended:
-                song_id += 1
-                playSong(song_id)
+                if current_mode == 0:
+                    song_id += 1
+                    if song_id >= len(song_list):
+                        song_id = 0
+                    playSong(song_id)
+                if current_mode == 1:
+                    queue_index += 1
+                    if queue_index >= len(song_queue):
+                        queue_index = 0
+                    playSong(song_queue[queue_index])
+
     except KeyboardInterrupt:
         return
 
+def load_playlist(id):
+    global playlists
+    return playlists[id]
+
 def handleConnection(conn : socket.socket, addr):
-    global p
+    global p, current_mode, playlist_name, song_queue, queue_index, playlists
     
     while True:
         global song_id
@@ -43,20 +67,44 @@ def handleConnection(conn : socket.socket, addr):
         command = values[0]
         args = values[1:]
 
-        
-
         if command == "get_current_status":
             print("Command: get_current_status")
-            current_song = song_list[song_id]
+            if current_mode == 0:
+                current_song = song_list[song_id]
+            elif current_mode == 1:
+                current_song = song_list[song_queue[queue_index]]
+
             if p:
                 is_playing = p.is_playing()
             else:
                 is_playing = 0
-            conn.send(f"{current_song}¬{is_playing}".encode())
+            conn.send(f"{json.dumps(current_song)}¬{is_playing}¬{current_mode}".encode())
+        
+        if command == "change_queue_index":
+            if current_mode == 1 and int(args[0]) < len(song_queue):
+                queue_index = int(args[0])
+                playSong(song_queue[queue_index])
+
+        if command == "get_queue":
+            conn.send(json.dumps(song_queue).encode())
+
+        if command == "get_playlists":
+            conn.send(json.dumps(playlists).encode())
+
         if command == "play":
+            current_mode = 0
             print("Command: play")
             song_id = int(args[0])
             playSong(song_id)
+
+        if command == "play_playlist":
+            current_mode = 1
+            queue_index = 0
+            playlist = load_playlist(int(args[0]))
+            song_queue = playlist["songs"]
+            playlist_name = playlist["name"]
+            playSong(song_queue[queue_index])
+
         if command == "pause":
             if p:
                 p.pause()
--- frontend/frontend.py
+++ frontend/frontend.py
@@ -11,12 +11,27 @@ s.connect(("127.0.0.1", 4444))
 def getCurrentStatus():
     s.send("get_current_status".encode())
     data = s.recv(1024).decode().split("¬")
-    return (json.loads(data[0].replace("'", "\"")), int(data[1]))
+    return (json.loads(data[0]), int(data[1]), int(data[2]))
 
+def getQueue():
+    s.send("get_queue".encode())
+    data = json.loads(s.recv(1024).decode())
+    return data
+
+@app.route("/playlists")
+def get_playlists():
+    s.send("get_playlists".encode())
+    data = json.loads(s.recv(1024).decode())
+    return render_template("playlists.html", playlists=data)
+
+@app.route("/play_playlist")
+def play_playlist():
+    s.send(f"play_playlist {request.args.get('id')}".encode())
+    return redirect("/")
 
 @app.route("/songs")
 def songs():
-    currentSong, _ = getCurrentStatus()
+    currentSong, _, _ = getCurrentStatus()
     s.send("get_songs".encode())
     data = json.loads(s.recv(4096).decode("utf-8"))
     return render_template("song_list.html", songs=data, currentSong=currentSong)
@@ -28,8 +43,22 @@ def pause():
 
 @app.route("/")
 def index():
-    currentSong, isPlaying = getCurrentStatus()
-    return render_template("index.html", currentSong=currentSong, isPlaying=isPlaying)
+    currentSong, isPlaying, currentMode = getCurrentStatus()
+    queue = []
+    if currentMode == 1:
+        s.send("get_songs".encode())
+        songs = json.loads(s.recv(4096).decode("utf-8"))
+        raw_queue = getQueue()
+        queue = []
+        for i in range(0, len(raw_queue)):
+            queue.append(songs[raw_queue[i]])
+    print(queue)
+    return render_template("index.html", currentSong=currentSong, isPlaying=isPlaying, currentMode=currentMode, queue=queue)
+
+@app.route("/change_queue_index")
+def change_queue_index():
+    s.send(f"change_queue_index {request.args.get('index')}".encode())
+    return redirect("/")
 
 @app.route("/play")
 def play_song():
